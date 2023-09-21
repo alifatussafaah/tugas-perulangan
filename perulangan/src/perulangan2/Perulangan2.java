abcec0dfba404e19155a16c3c7bca0e3f9f94231
alifatussafaah <145674416+alifatussafaah@users.noreply.github.com>
2023-09-21 21:46:13 +0700

    Add files via upload

---
+++ perulangan/src/perulangan2/Perulangan2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package perulangan2;
+
+/**
+ *
+ * @author DELL
+ */
+public class Perulangan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // perulangan menampilkan angka 
+        for (int i=1;i<=5; i++){
+            System.out.println("Iterasi ke-" +i);
+        }
+    }
+}
